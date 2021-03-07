3838ab96100a352ca7d38a9f63907f41093d9506
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-07 19:38:06 +0200

    initial actix scafolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use listenfd::ListenFd;
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    "hello"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| App::new().service(greet));
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind("127.0.0.1:3000")?,
+    };
+
+    server.run().await
 }
