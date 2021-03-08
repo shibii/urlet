feb20ea2748d345a3a909f46b0dc256d9135bf96
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-08 12:58:10 +0200

    improvet dev workflow with listenfd

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
 use actix_web::{get, App, HttpServer, Responder};
-use dotenv_parser::parse_dotenv;
 use listenfd::ListenFd;
-use std::collections::BTreeMap;
-use std::fs;
+
+mod dotenv;
 
 #[get("/")]
-async fn greet() -> impl Responder {
-    "hello"
-}
-
-fn get_env() -> BTreeMap<String, String> {
-    let env_file = fs::read_to_string(".env").expect(".env file not found");
-    parse_dotenv(&env_file).expect("failed to parse .env file")
+async fn greet(req_body: String) -> impl Responder {
+    format!("hello {}", req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::parse();
+
     let mut listenfd = ListenFd::from_env();
-    let env = get_env();
     let mut server = HttpServer::new(|| App::new().service(greet));
 
-    let bind_addr = env
-        .get("SERVER_BIND_ADDRESS")
-        .expect("missing SERVER_BIND_ADDRESS env var");
+    let bind_addr = std::env::var("SERVER_BIND_ADDRESS")
+        .expect("missing environment variable: SERVER_BIND_ADDRESS");
 
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
