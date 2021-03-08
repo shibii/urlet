447bbce6308784ea35348b1bae27e42a4d72937a
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-08 15:33:22 +0200

    added a route to generate new urlets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{App, HttpServer};
 use listenfd::ListenFd;
+use tracing::{event, Level};
 
+mod db;
 mod dotenv;
 mod routes;
 
@@ -8,9 +10,21 @@ mod routes;
 async fn main() -> std::io::Result<()> {
     dotenv::parse();
     tracing_subscriber::fmt::init();
-
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().service(routes::greet));
+
+    let db_addr =
+        std::env::var("DATABASE_URL").expect("missing environment variable: DATABASE_URL");
+
+    event!(Level::INFO, "connecting to database");
+    let pool = db::connect(&db_addr)
+        .await
+        .expect("failed to connect to the database");
+
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(routes::generate_urlet)
+    });
 
     let bind_addr = std::env::var("SERVER_BIND_ADDRESS")
         .expect("missing environment variable: SERVER_BIND_ADDRESS");
--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,31 @@
-use actix_web::{get, Responder};
+use actix_web::{post, web, HttpResponse, Responder};
+use serde::Serialize;
+use sqlx::postgres::PgPool;
 use tracing::{event, instrument, Level};
+use uuid::Uuid;
 
-#[instrument(name = "get /")]
-#[get("/")]
-pub async fn greet(req_body: String) -> impl Responder {
-    event!(Level::INFO, "get /");
-    format!("hello {}", req_body)
+#[derive(Serialize)]
+struct Urlet {
+    id: Uuid,
+    url: String,
+}
+
+#[instrument(skip(pool))]
+#[post("/")]
+pub async fn generate_urlet(url: String, pool: web::Data<PgPool>) -> impl Responder {
+    let id = Uuid::new_v4();
+    event!(Level::INFO, %id, %url, "inserting a new urlet into the database");
+    let res = sqlx::query_as!(
+        Urlet,
+        r#"INSERT INTO urlet(id, url) VALUES ( $1, $2 ) RETURNING *"#,
+        id,
+        url
+    )
+    .fetch_one(pool.get_ref())
+    .await;
+
+    match res {
+        Ok(urlet) => HttpResponse::Ok().json(urlet).await,
+        _ => HttpResponse::InternalServerError().finish().await,
+    }
 }
