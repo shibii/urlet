b623c675203c792043fbb1ddcdc7161c7dc5ccf2
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-08 13:00:46 +0200

    added logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use actix_web::{get, App, HttpServer, Responder};
 use listenfd::ListenFd;
+use tracing::{event, instrument, Level};
 
 mod dotenv;
 
+#[instrument(name = "get /")]
 #[get("/")]
 async fn greet(req_body: String) -> impl Responder {
+    event!(Level::INFO, "get /");
     format!("hello {}", req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::parse();
+    tracing_subscriber::fmt::init();
 
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(|| App::new().service(greet));
