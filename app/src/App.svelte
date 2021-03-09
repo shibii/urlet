e01a88312bcd47760571d15356b03ff231d63952
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-10 00:34:07 +0200

    svelte project from snowpack template

---
+++ app/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  let url;
+  const submit = () => {};
+</script>
+
+<form on:submit|preventDefault={submit}>
+  <input type="text" bind:value={url} />
+  <input type="submit" value="generate" />
+</form>
