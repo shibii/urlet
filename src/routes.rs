0ec83f59cd8de419595fb3a89b5a076784328087
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-09 20:11:06 +0200

    added tracing macros to routes

--- src/routes.rs
+++ src/routes.rs
@@ -14,20 +14,24 @@ struct Urlet {
 #[instrument(skip(req, pool))]
 #[get("/{id}")]
 async fn redirect(req: HttpRequest, pool: web::Data<PgPool>) -> impl Responder {
+    event!(Level::TRACE, "getting route parameter id");
     let id = match req.match_info().get("id") {
         Some(id) => id,
         _ => return HttpResponse::BadRequest().finish(),
     };
 
+    event!(Level::INFO, %id, "decoding id shorthand into uuid");
     let uuid = match super::urlet::decode(id) {
         Ok(uuid) => uuid,
         _ => return HttpResponse::BadRequest().finish(),
     };
 
+    event!(Level::INFO, %id, %uuid, "querying database for id param matching urlet");
     let res = sqlx::query_as!(Urlet, r#"SELECT * FROM urlet WHERE id = $1"#, uuid)
         .fetch_one(pool.get_ref())
         .await;
 
+    event!(Level::TRACE, "responding to request");
     match res {
         Ok(urlet) => HttpResponse::PermanentRedirect()
             .header("Location", urlet.url)
@@ -54,6 +58,7 @@ async fn generate_urlet(url: String, pool: web::Data<PgPool>) -> impl Responder 
 
     let urlet = super::urlet::encode(uuid);
 
+    event!(Level::TRACE, "responding to request");
     match res {
         Ok(_) => HttpResponse::Ok().json(urlet),
         _ => HttpResponse::InternalServerError().finish(),
