c0469ce4e9ce4bb0b01898ae1ad6a5320bf5989e
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-11 12:46:39 +0200

    proxy api calls to api server

--- app/snowpack.config.js
+++ app/snowpack.config.js
@@ -1,4 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
+
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createServer({ target: 'http://localhost:3000/' });
+
 module.exports = {
   mount: {
     public: {url: '/', static: true},
@@ -11,6 +15,14 @@ module.exports = {
   routes: [
     /* Example: Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {
+      match: "all",
+      src: '/api/.*',
+      dest: (req, res) => {
+        req.url = req.url.replace(/^\/api/, '');
+        proxy.web(req,res);
+      }
+    }
   ],
   optimize: {
     /* Example: Bundle your final build: */
