909894287c937762d6d5b7d376f993b482ad07fc
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-11 12:50:37 +0200

    trivial test to check api routing in dev env

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+  import { generate_urlet } from "./services/api";
+
   let url;
-  const submit = () => {};
+
+  const generate = () => {
+    generate_urlet(url)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 </script>
 
-<form on:submit|preventDefault={submit}>
+<form on:submit|preventDefault={generate}>
   <input type="text" bind:value={url} />
   <input type="submit" value="generate" />
 </form>
