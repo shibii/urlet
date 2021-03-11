24ce974f39a1389ef7b622b11ae60e01a0eb3d0c
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-11 13:27:59 +0200

    client-side url validation

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,6 +2,15 @@
   import { generate_urlet } from "./services/api";
 
   let url;
+  let valid;
+  $: {
+    try {
+      let test_url = new URL(url);
+      valid = test_url.protocol.match(/https?:/) ? true : false;
+    } catch (err) {
+      valid = false;
+    }
+  }
 
   const generate = () => {
     generate_urlet(url)
@@ -12,5 +21,5 @@
 
 <form on:submit|preventDefault={generate}>
   <input type="text" bind:value={url} />
-  <input type="submit" value="generate" />
+  <input type="submit" value="generate" disabled={!valid} />
 </form>
