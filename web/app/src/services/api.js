ac9c53af1f507d09fc7d01c7a71982e0dff457d2
Seppo Kilpeläinen <seppo.kilpelainen@gmail.com>
2021-03-13 15:59:14 +0200

    moved api route to domain root

--- web/app/src/services/api.js
+++ web/app/src/services/api.js
@@ -3,5 +3,5 @@ import ky from "ky";
 const api = ky.create();
 
 export const generate_urlet = (url) => {
-  return api.post("/api/", {body: url}).json();
+  return api.post("/", {body: url}).json();
 };
